19bc10bfa1acd28bf8b98c9998fa8f05306cd8a5
TrevorKong <35690612+trevorkong@users.noreply.github.com>
2018-12-03 16:56:33 -0500

    Revert "phoneCall"

--- app/src/androidTest/java/com/example/massyon/care_portal/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/massyon/care_portal/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.massyon.phonecall;
+package com.example.massyon.care_portal;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.massyon.phonecall", appContext.getPackageName());
+        assertEquals("com.example.massyon.care_portal", appContext.getPackageName());
     }
 }
