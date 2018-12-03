fdee25c2c11bafb24404423e15053c74a9127d37
YahuiWen <wenyahui1997@gmail.com>
2018-12-03 17:56:56 -0500

    ..

---
+++ app/src/main/java/com/example/wenyahui/howardcenter/healthActivity.java
@@ -0,0 +1,10 @@
+package com.example.wenyahui.howardcenter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class healthActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_main);
+    }
+}
