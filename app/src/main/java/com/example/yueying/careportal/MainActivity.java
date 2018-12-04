10f587d25f7778a0372eb6be1404d163fd84df02
Yueying Zhao <yueying.zhao@uvm.edu>
2018-12-04 16:09:41 -0500

    update

--- app/src/main/java/com/example/yueying/careportal/MainActivity.java
+++ app/src/main/java/com/example/yueying/careportal/MainActivity.java
@@ -3,49 +3,34 @@ package com.example.yueying.careportal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button help;
-    private Button news;
-    private Button health;
+    private CardView help;
+    private CardView news;
+    private CardView health;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        help = (Button) findViewById(R.id.help_button);
-        help.setOnClickListener(new View.OnClickListener() {
+
+        news = (CardView) findViewById(R.id.news);
+        findViewById(R.id.news).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, YourActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-        news = (Button) findViewById(R.id.news_button);
-        news.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, YourActivity.class);
+                Intent intent = new Intent(MainActivity.this, NewsActivity.class);
                 startActivity(intent);
 
             }
         });
 
-        health = (Button) findViewById(R.id.health_button);
-        health.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, YourActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
+
 }
+
